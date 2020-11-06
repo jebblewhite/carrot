c13c35e17e9e79b28f35a6d52a0288cf79304416
Jason Ebblewhite <jasonebblewhite@gmail.com>
2020-11-06 11:03:30 +0000

    carrots lol

---
+++ test.py
@@ -0,0 +1,3 @@
+""" 
+Here is my data science playground. I intend to perform basic data manipulation and visualisation of world carrot production data. This directory will be hosted on my public github.
+"""
