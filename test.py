74a6d0c97f0943d4904eeab417bf467f38945b5e
Jason Ebblewhite <jasonebblewhite@gmail.com>
2020-11-07 13:40:29 +0000

    made some carrot based graphs, almost readable

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 """ 
 Here is my data science playground. I intend to perform basic data manipulation and visualisation of world carrot production data. This directory will be hosted on my public github.
+🥕
 """
+
+
+import seaborn as sn
+import pandas as pd
+
+df = pd.read_csv("carrot_data.csv")
+
+df
+
+
